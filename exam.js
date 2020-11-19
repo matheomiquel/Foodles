3a2c98953bd37a28bc2f71d89db7e7be7a4ca030
Mathéo <matheo.miquel@gmail.com>
2020-11-19 15:43:38 +0100

    all

--- exam.js
+++ exam.js
@@ -10,13 +10,12 @@ const count = function (sentence, n) {
             count.push(1);
         }
     })
-    words.map((word, index) => {
+    words.map(word => {
         data.push({
             word: word,
             count: count[words.indexOf(word)]
         })
     })
-    let string = ""
     let result = data.sort((a, b) => {
         return a.count - b.count;
     }).reverse();
@@ -25,6 +24,21 @@ const count = function (sentence, n) {
             return a - b
         }
     })
+    let bool = true;
+    while (bool) {
+        bool = false;
+        for (let i = 0; i < data.length; i++) {
+            if (data[i + 1])
+                if (data[i].count === data[i + 1].count) {
+                    if (data[i].word.localeCompare(data[i + 1].word) > 0) {
+                        const tmp = data[i].word;
+                        data[i].word = data[i + 1].word;
+                        data[i + 1].word = tmp;
+                    }
+                }
+        }
+    }
+    let string = ""
     for (let i = 0; i < n; i++)
         string += (`${data[i].word} : ${data[i].count}\n`)
     return string
--- test.js
+++ test.js
@@ -1,5 +1,12 @@
-import {count} from "./exam.js"
+import {
+    count
+} from "./exam.js"
 
 
-count("baz bar foo foo zblah zblah zblah baz toto bar", 3);
-console.log(count("baz bar foo foo zblah zblah zblah baz toto bar", 3));
+//count("???", 3);
+
+import assert from "assert"
+it('count word', (resolve) => {
+    assert.strictEqual(count("baz bar foo foo zblah zblah zblah baz toto bar", 3), `zblah : 3\nbar : 2\nbaz : 2\n`);
+    resolve();
+});
